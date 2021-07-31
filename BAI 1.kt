573d69543a330efcc246072bdffc54a1993db0dc
ductam7415 <83055888+ductam7415@users.noreply.github.com>
2021-07-31 10:34:29 +0700

    Add files via upload

---
+++ BAI 1.kt
@@ -0,0 +1,11 @@
+fun main(){
+    var a : Int = 0
+    var b : Int = 0
+
+    a = readLine()!!.toInt()
+    b = readLine()!!.toInt()
+
+    println("Chu vi của hình chữ nhật là " + (a+b)*2)
+    println("Diện tích của hình chữ nhật là " + a*b)
+    
+}
